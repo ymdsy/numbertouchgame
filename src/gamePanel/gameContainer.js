ebf5ce5332395b28ea04ceac0d78e18f368b3247
yamada <yamada@example.com>
2019-07-24 23:56:03 +0900

    add render panel method and memoize

--- src/gamePanel/gameContainer.js
+++ src/gamePanel/gameContainer.js
@@ -1,27 +1,19 @@
 import React from "react";
 import GamePresenter from "./gamePresenter.js";
-// import memoize from "memoize-one";
+import memoize from "memoize-one";
+import isEqual from "lodash/isEqual";
+
 export class GameContainer extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      panels: []
+      correctArray: []
     };
   }
 
   /** indexからパネルの合計枚数を計算する */
   getPanelNum(index) {
     return Math.pow(index + 3, 2);
-  }
-
-  /**
-   * パネルの配列を生成する。
-   *
-   */
-  createPanels(levelIndex) {
-    const panelNum = Math.pow(levelIndex + 3, 2);
-    const panels = [...Array(panelNum).keys()].map(i => i + 1);
-    return this.shuffleArray(panels);
   }
 
   /**
@@ -35,22 +27,34 @@ export class GameContainer extends React.PureComponent {
       .map(a => a[1]);
   }
 
-  render() {
-    // 過去の枚数と変化が起きたときのみパネルを生成する
-    if (
-      this.state.panels.length !==
-      this.getPanelNum(this.props.currentLevelIndex)
-    ) {
-      this.setState({
-        panels: this.createPanels(this.props.currentLevelIndex)
-      });
-    }
+  /**
+   * 回答となる配列を取得する
+   */
+  getCorrectArray(index) {
+    return [...Array(this.getPanelNum(index)).keys()].map(i => i + 1);
+  }
 
+  /**
+   * ボタンがクリックされた時に、ゲームを行う
+   *
+   */
+  onClick(value) {
+    // ここに
+  }
+
+  /**
+   *    パネルのインデックスを取得して、ランダムに並べられたパネルの配列を種痘する
+   * 第二引数はDeepEqualをするための関数
+   */
+  renderPanels = memoize(levelIndex => {
+    const panels = this.getCorrectArray(levelIndex);
+    return this.shuffleArray(panels);
+  }, isEqual);
+
+  render() {
+    const panels = this.renderPanels(this.props.currentLevelIndex);
     return (
-      <GamePresenter
-        panelNum={this.props.currentLevelIndex}
-        panels={this.state.panels}
-      />
+      <GamePresenter panelNum={this.props.currentLevelIndex} panels={panels} />
     );
   }
 }
